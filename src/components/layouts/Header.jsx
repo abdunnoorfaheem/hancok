9ae6f5b7e90a655c258bf882c26abc638c4546c3
abdunnoorfaheem <abdunnoor3030@gmail.com>
2025-08-25 02:11:41 +0600

    header section added

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,18 +1,49 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "../Container";
+import Heading from "../Heading";
+import Flex from "../Flex";
+import { CiGlobe } from "react-icons/ci";
+import { FaSignOutAlt,FaSearch  } from "react-icons/fa";
 
 const Header = () => {
   return (
     <>
-      <div className="">
-        <ul className="flex gap-x-12">
-            <li><Link to={"/"}>Home</Link></li>
-            <li><Link to={"/aboutUs"}>About Us</Link></li>
+      <div className="py-[30px] bg-black text-white">
+        <Container>
+          <Flex className={"justify-between"}>
+          <Heading text={"Hancok"} tagName={"h3"} className={""}/>
+          <div className="">
+            <ul className="flex gap-x-12">
+            <li>
+              <Link to={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link to={"/aboutUs"}>About Us</Link>
+            </li>
             <li>Tornament</li>
-        </ul>
+            <li>Store</li>
+            <li>Team</li>
+            <li>FAQ</li>
+            <li>Contact Us</li>
+          </ul>
+          </div>
+          <div className="text-[20px]">
+           <div className="flex items-center gap-x-6">
+            <CiGlobe/>
+           <div className="flex items-center gap-1">
+            <FaSignOutAlt/>
+            <h4>Sign In</h4>
+           </div>
+           <FaSearch />
+           </div>
+          </div>
+          </Flex>
+         
+        </Container>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
